2ced5388b27f2764d711323ae92f6a1f98cbd7f4
Sebastián Cabezas Ríos <68340396+sebastiancabezasrios@users.noreply.github.com>
2023-03-29 22:02:44 -0300

    Add files via upload

---
+++ Modulo 02/EjercicioVentas/Vendedor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using EjercicioVentas;
+
+namespace EjercicioVentas
+{
+    public class Vendedor : Persona
+    {
+        private int tiendaId;
+
+        public Vendedor(int _id, string _rut, string _nombre, string _apellido, int _tiendaId) : base(_id, _rut, _nombre, _apellido)
+        {
+            tiendaId = _tiendaId;
+        }
+
+        public int getTiendaId()
+        {
+            return tiendaId;
+        }
+    }
+}
+
---
+++ Modulo 02/EjercicioVentas/Venta.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace EjercicioVentas
+{
+    public class Venta
+    {
+        private int id;
+        private string fecha;
+        private int tiendaId;
+        private int vendedorId;
+        private int clienteId;
+        private int medioPagoId;
+        private bool activo;
+
+        public Venta(int _id, string _fecha, int _tiendaId, int _vendedorId, int _clienteId, int _medioPagoId)
+        {
+            id = _id;
+            fecha = _fecha;
+            tiendaId = _tiendaId;
+            vendedorId = _vendedorId;
+            clienteId = _clienteId;
+            medioPagoId = _medioPagoId;
+            activo = true;
+        }
+
+        public int getTiendaId()
+        {
+            return tiendaId;
+        }
+        public void setActivo(bool _activo)
+        {
+            activo = _activo;
+        }
+        public bool isActivo()
+        {
+            return activo;
+        }
+
+        
+    }
+
+
+}
+
