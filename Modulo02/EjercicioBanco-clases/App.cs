7e66a8f3faf72e11d2fa74729748c52aa70b0719
Sebastián Cabezas Ríos <68340396+sebastiancabezasrios@users.noreply.github.com>
2023-03-30 00:19:26 -0300

    Add files via upload

---
+++ Modulo02/EjercicioBanco-clases/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace EjercicioBanco_clases
+{
+	public class App
+	{
+		public static void Main(String[] args)
+		{
+			Console.WriteLine("Ejercicio Banco En Clases");
+		}
+	}
+}
+
