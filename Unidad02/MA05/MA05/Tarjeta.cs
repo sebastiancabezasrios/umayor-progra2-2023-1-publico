211701dfb1f93530ab87e240d7a7723f42b8b396
Sebastián Cabezas Ríos <68340396+sebastiancabezasrios@users.noreply.github.com>
2023-04-17 19:44:53 -0400

    agregar archivos unidad 2

---
+++ Unidad02/MA05/MA05/Tarjeta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace MA05
+{
+	public class Tarjeta
+	{
+		private int id;
+		private double numero;
+		private string nombreBanco;
+
+		public Tarjeta()
+		{
+
+		}
+	}
+}
+
---
+++ Unidad02/MA05/MA05/TarjetaCredito.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace MA05
+{
+	public class TarjetaCredito:Tarjeta
+	{
+		private string nombreTarjeta;
+		private int cupo;
+		private int consumo;
+
+		public TarjetaCredito()
+		{
+		}
+
+		public bool comprar(int _valor)
+		{
+			if ((cupo - consumo) <= _valor)
+			{
+				consumo -= _valor;
+				return true;
+			}
+			return false;
+		}
+
+		public bool pagarTarjeta(int _monto)
+		{
+			if (_monto <= consumo)
+			{
+				consumo -= _monto;
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
